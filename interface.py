24dac54af6fd31126bc10e4dd869c361b472b832
DarekBx <darek.baranczuk@gmail.com>
2020-09-08 16:09:56 +0200

    Added mappings, fillRect method

--- interface.py
+++ interface.py
@@ -1,16 +1,55 @@
+'''
+ - Show lock Screen (with PIN) after start, after unlock main screen is shown
+ - Main screen
+   - Top menu: Menu, Lock, Wifi Status
+   - Menu options:
+     - Scanning Device tester
+	 - BT Scanner/Sniffer
+	 - Wifi options
+	 - File manager
+	 - RSS reader
+
+'''
 import time
 
 try:
-    from machine import LCD
+    from machine import LCD, Map, Pin
 except ModuleNotFoundError:
     print("Using virtual machine")
-    from virtual_machine import LCD
+    from virtual_machine import LCD, Map, Pin
 
 class MainMenu:
 
 	def draw(self):
 		lcd = LCD()
 		lcd.fillScreen(LCD.color.BLACK)
-		lcd.setTextColor(LCD.color.WHITE, LCD.color.BLACK)
-		lcd.drawString("RPI Monitor2", 50, 50)
-		lcd.drawLine(int(10), int(10), int(50), int(50), LCD.color.WHITE)
+		lcd.setTextColor(LCD.color.WHITE, LCD.color.DARKGREY)
+
+		lcd.fillRect(0, 0, 180, 20, LCD.color.DARKGREY)
+		lcd.drawLine(int(60), int(0), int(60), int(20), LCD.color.WHITE)
+		lcd.drawLine(int(120), int(0), int(120), int(20), LCD.color.WHITE)
+		lcd.drawLine(int(180), int(0), int(180), int(20), LCD.color.WHITE)
+		lcd.drawLine(int(0), int(20), int(180), int(20), LCD.color.WHITE)
+
+		#button = Pin(Map.WIO_KEY_B, Pin.IN, Pin.PULL_UP)
+
+		lcd.drawString("MENU", 5, 5)
+		lcd.drawString("LOCK", 65, 5)
+		lcd.drawString("WIFI", 125, 5)
+
+		self.dump(lcd)
+
+	def dump(self, lcd):
+		x = 5
+		y = 25
+		for c in dir(Map):
+			value = getattr(Map, c)
+			lcd.drawString("{0}: {1}".format(c, value), x, y)
+			y += 15
+			if y > 200:
+				y = 25
+				x += 100
+
+
+
+
--- virtual_machine.py
+++ virtual_machine.py
@@ -12,8 +12,25 @@ class LCD:
 	
 	class color:
 		BLACK = (0, 0, 0)
+		BLUE = (0, 0, 255)
+		CYAN = (0, 238, 238)
+		DARKCYAN = (0, 139, 139)
+		DARKGREEN = (0, 100, 0)
+		DARKGREY = (100, 100, 100)
+		GREEN = (0, 200, 0)
+		GREENYELLOW = (173, 255, 47)
+		LIGHTGREY = (211, 211, 211)
+		MAGENTA = (255, 0, 255)
+		MAROON = (255, 52, 179)
+		NAVY = (0, 0, 128)
+		OLIVE = (107, 142, 35)
+		ORANGE = (255, 165, 0)
+		PINK = (255, 192, 203)
+		PURPLE = (102, 0, 102)
+		YELLOW = (238, 238, 0)
 		WHITE = (255, 255, 255)
-
+		RED = (255, 0, 0)
+		
 	FPS = 3
 
 	width = 320
@@ -59,19 +76,19 @@ class LCD:
 		self._applyRotation()
 
 	def color16to8(self, color):
-		pass
+		raise Exception("Not implemented")
 
 	def getCursorX(self):
-		pass
+		raise Exception("Not implemented")
 
 	def getCursorY(self):
-		pass
+		raise Exception("Not implemented")
 
 	def getRotation(self):
-		pass
+		raise Exception("Not implemented")
 
 	def getTextDatum(self):
-		pass
+		raise Exception("Not implemented")
 
 	def setRotation(self, r):
 		"""Set virtual display rotation
@@ -91,91 +108,92 @@ class LCD:
 		self._textBgColor = bgColor
 
 	def drawCentreString(self, string, dX, poY, font = None):
-		pass
+		raise Exception("Not implemented")
 
 	def drawString(self, string, poX, poY, font = None): 
 		text_surface = self._font.render(string, True, self._textColor)
+		text_width, text_height = self._font.size(string)
+		pygame.draw.rect(self._screen, self._textBgColor, [poX, poY, text_width, text_height]) 
 		self._screen.blit(text_surface, dest=(poX,poY))
 
 	def drawChar(self, char, poX, poY):
-		pass
+		raise Exception("Not implemented")
 
 	def drawCircle(self, x0, y0, r, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawCircleHelper(self, x0, y0, r, cornername, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawEllipse(self, x, y, rx, ry, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawFastHLine(self, x, y, w, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawFastVLine(self, x, y, h, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawFloat(self, floatNumber, decimal, x, y , font):
-		pass
+		raise Exception("Not implemented")
 
 	def drawLine(self, x, y, x1, y1, color):
 		pygame.draw.line(self._screen, color, (x, y), (x1, y1))
 
 	def drawNumber(self, number, x, y, font):
-		pass
+		raise Exception("Not implemented")
 
 	def drawPixel(self, x, y, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawRect(self, x, y, w, h, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawRoundRect(self, x, y, w, h, r, color):
-		pass
+		raise Exception("Not implemented")
 
 	def drawTriangle(self, x0, y0, x1, y1, x2, y2, color):
-		pass
+		raise Exception("Not implemented")
 
 	def fillCircle(self, x0, y0, r, color):
-		pass
+		raise Exception("Not implemented")
 
 	def fillCircleHelper(self, x0, y0, r, cornername, color):
-		pass
+		raise Exception("Not implemented")
 
 	def fillEllipse(self, x, y, rx, ry, color):
-		pass
+		raise Exception("Not implemented")
 
 	def fillRect(self, x, y, w, h, color):
-		pass
+		pygame.draw.rect(self._screen, color, [x, y, w, h])
 
 	def fillRoundRect(self, x, y, w, h, r, color):
-		pass
+		raise Exception("Not implemented")
 
 	def fillTriangle(self, x0, y0, x1, y1, x2, y2, color):
-		pass
+		raise Exception("Not implemented")
 
 	def fillScreen(self, color):
 		self._screenColor = color
-		pass
 
 	def invertDisplay(self, n):
-		pass
+		raise Exception("Not implemented")
 
 	def setPivot(self, x, y):
-		pass
+		raise Exception("Not implemented")
 
 	def setTextDatum(self, datum):
-		pass
+		raise Exception("Not implemented")
 
 	def setTextFont(self, font):
-		pass
+		raise Exception("Not implemented")
 
 	def setTextSize(self, size):
 		self._fontSize = size
 		self._font = pygame.font.Font(pygame.font.get_default_font(), self._fontSize)
 
 	def textWidth(self, string):
-		pass
+		raise Exception("Not implemented")
 
 	def _applyRotation(self):
 		if self._rotation is not None:
@@ -207,11 +225,19 @@ class Pin:
 		self._mode = mode
 		
 	def on(self):
-		pass
+		raise Exception("Not implemented")
 	
 	def off(self):
-		pass
+		raise Exception("Not implemented")
 
 class Map:
 
-	LED_BUILTIN = 13
+	LED_BUILTIN = 50
+	WIO_KEY_A = 55
+	WIO_KEY_B = 56
+	WIO_KEY_C = 57
+	WIO_5S_LEFT = 59
+	WIO_5S_RIGHT = 60
+	WIO_5S_UP = 58
+	WIO_5S_DOWN = 61
+	WIO_5S_PRESS = 62
